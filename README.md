260f6066359a4de90bebf818cfa191676421b00c
Thomas Hangstörfer <thomas@bitcoder.de>
2017-11-19 21:12:23 +0100

    cam-image-server can now provide older images

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# cam-image-server
-Small web-server that returns the latest image of a surveillance cam
+
+# cam-image-server.js
+
+Usage:
+   nodejs cam-image-server.js
+
+Provides access to the latest image of the surveillance camera
+
+
+http://server:9615/latest.jpg
+http://server:9615/latest
+
+Or, with a given index, to previous images:
+
+http://server:9615/latest-1.jpg -> image before the latest
+http://server:9615/latest-2.jpg -> 2 images before the latest
--- cam-image-server.js
+++ cam-image-server.js
@@ -1,3 +1,18 @@
+
+// cam-image-server.js
+//
+// Usage:
+//    nodejs cam-image-server.js
+//
+// Provides access to the latest image of the surveillance camera
+// http://server:9615/latest.jpg
+// http://server:9615/latest
+//
+// Or, with a given index, to previous images:
+// http://server:9615/latest-1.jpg -> image before the latest
+// http://server:9615/latest-2.jpg -> 2 images before the latest
+
+
 var http = require('http')
 var url = require('url')
 var fs = require('fs')
@@ -8,7 +23,7 @@ var cam_image_path = '/qnap/Download/today'
 var port = 9615
 
 
-function getLatest() {
+function getLatest(index) {
     fs.realpath(cam_image_path, function(err, path) {
         if (err) {
             console.log(err);
@@ -17,13 +32,19 @@ function getLatest() {
         console.log('Path is : ' + path);
     });
 
-    var filelist = fs.readdirSync(cam_image_path)
+    if (typeof index == 'undefined') {
+        //console.log('undefined!');
+        index = 0;
+    }
+    console.log('index: ' + index);
+
+    var filelist = fs.readdirSync(cam_image_path);
 
     filelist.forEach(function(file, i) {
         //console.log('File: %d: %s', i, file);
-	//console.log('indexOf = ' + file.indexOf('cam-201') );
+        //console.log('indexOf = ' + file.indexOf('cam-201') );
         if (file.indexOf('cam-201') != 0) {
-            filelist.splice(i, 1)
+            filelist.splice(i, 1);
             //console.log('splice ' + i + ' - ' + file )
         }
     });
@@ -33,43 +54,69 @@ function getLatest() {
     //filelist.forEach(file => {
     //    console.log('File: ' + file);
     //});
-    console.log('latest: ' + cam_image_path + "/" + filelist[filelist.length - 1])
-    return cam_image_path + "/" + filelist[filelist.length - 1];
+    var fileListIndex = filelist.length - index - 1;
+    console.log('latest: ' + cam_image_path + "/" + filelist[fileListIndex]);
+    return cam_image_path + "/" + filelist[fileListIndex];
 }
 
 
 http.createServer(function(request, response) {
     try {
-        var requestUrl = url.parse(request.url)
+        var requestUrl = url.parse(request.url);
 
         // need to use path.normalize so people can't access directories underneath baseDirectory
-        var fsPath = baseDirectory + path.normalize(requestUrl.pathname)
+        var fsPath = baseDirectory + path.normalize(requestUrl.pathname);
 
         //console.log("fsPath = " + fsPath)
         //console.log("normalize = " + path.normalize(requestUrl.pathname))
         //console.log("pathname = " + requestUrl.pathname)
 
-        if (requestUrl.pathname == "/latest" || requestUrl.pathname == "/latest.jpg" ) {
-            fsPath = getLatest()
+        var index = getIndex(requestUrl.pathname);
+        //if (requestUrl.pathname == "/latest" || requestUrl.pathname == "/latest.jpg") {
+        if (requestUrl.pathname.startsWith( "/latest" ) ) {
+            fsPath = getLatest(index);
             response.setHeader('Content-Type', 'image/jpeg');
-            console.log("LATEST")
+            console.log("LATEST");
         }
 
-        var fileStream = fs.createReadStream(fsPath)
-        fileStream.pipe(response)
+        var fileStream = fs.createReadStream(fsPath);
+        fileStream.pipe(response);
         fileStream.on('open', function() {
-            response.writeHead(200)
+            response.writeHead(200);
         })
         fileStream.on('error', function(e) {
-            response.writeHead(404) // assume the file doesn't exist
-            response.end()
+            response.writeHead(404); // assume the file doesn't exist
+            response.end();
         })
     } catch (e) {
-        response.writeHead(500)
-        response.end() // end the response so browsers don't hang
-        console.log(e.stack)
+        response.writeHead(500);
+        response.end(); // end the response so browsers don't hang
+        console.log(e.stack);
     }
-}).listen(port)
+}).listen(port);
 
-getLatest()
-console.log("listening on port " + port)
+function getIndex(pathname) {
+    var index = 0;
+    try {
+        const exp = /\/latest-(\d+).jpg/;
+        var match = exp.exec(pathname);
+        if (match) {
+            console.log('match: ' + match[1]);
+            index = parseInt(match[1]);
+        }
+    } catch (e) {
+        console.log('getIndex(): ', e.stack);
+    }
+    console.log('getIndex(' + pathname + '): ' + index);
+    return index;
+}
+
+//getLatest();
+//getLatest(3);
+console.log("listening on port " + port);
+
+//getIndex("/latest");
+//getIndex("/latest.jpg");
+//getIndex("/latest-1.jpg");
+//getIndex("/latest-11.jpg");
+//getIndex("/latest-111.jpg");
